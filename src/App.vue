de9a60d4a36f0e45af86147ff9cd343b86870b06
Solaris86 <nikola.suscevic@gmail.com>
2019-01-21 11:04:53 +0100

    Basic vuex features

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
                 <app-counter></app-counter>
+                <br>
+                <app-another-counter></app-another-counter>
             </div>
         </div>
     </div>
@@ -13,12 +16,16 @@
 
 <script>
     import Counter from './components/Counter.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         components: {
             appCounter: Counter,
+            appAnotherCounter: AnotherCounter,
             appResult: Result,
+            appAnotherResult: AnotherResult
         }
     }
 </script>
